49e17241b57e09abf5de368aebdf680998ce1c07
Tatibnomb <49379010@est.ort.edu.ar>
2025-06-19 10:37:05 -0300

    Repaso For y Arrays terminado

---
+++ Assets/IntroArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroArray : MonoBehaviour
+{
+    public int[] edades;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Escribir el primer elemento del array
+        Debug.Log(edades[0]);
+
+        // Escribir la cantidad de elementos del array
+        Debug.Log(edades.Length);
+
+        // Escribir el último elemento del array
+        Debug.Log(edades[edades.Length - 1]);
+
+        // Escribir todos los elementos del array
+        for (int i = 0; i < edades.Length; i++)
+        {
+            Debug.Log(edades[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/RepasoFor.cs
+++ Assets/RepasoFor.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class RepasoFor : MonoBehaviour
 {
+    public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // Escribir en consola desde el 0 hasta el 4
+        // for (int i = 0; i < 5; i++)
+        // {
+        // Debug.Log(i);
+        // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // escribir los multiplos de 3 hasta el 30 inclusive
+        // for (int i = 3; i <= 30; i += 3)
+        // {
+        // Debug.Log(i);
+        // }
+
+        // Escribir los enteros desde y hasta dos numeros ingresados por inspector
+        // for (int i = num1; i <= num2; i++)
+        // {
+        // Debug.Log(i);
+        // }
+
+        // Escribir los enteros desde 25 hasta 4 exepto el 13
+        for (int i = 25; i >= 4; i--)
+        {
+            if (i == 13)
+            {
+                continue;
+            }
+            Debug.Log(i);
+        }
     }
 }
