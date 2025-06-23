3aabbfbafb429b1b8bea2810db9aef48bee2c75a
Tatibnomb <49379010@est.ort.edu.ar>
2025-06-23 09:52:58 -0300

    TP 5 Ítems 1, 2 y 3 terminados

---
+++ Assets/Scripts/ArrayFamilia.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayFamilia : MonoBehaviour
+{
+    public string[] integrantesFamilia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Escribir el primer elemento del array
+        Debug.Log(integrantesFamilia[0]);
+    
+        // Escribir la cantidad de elementos del array
+        Debug.Log(integrantesFamilia.Length);
+
+        // Escribir todos los elementos del array
+        for (int i = 0; i < integrantesFamilia.Length; i++)
+        {
+            Debug.Log(integrantesFamilia[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
