91d1dba949ebc26daf5a1cee7c32f815078b6aef
Tatibnomb <49379010@est.ort.edu.ar>
2025-06-23 19:18:36 -0300

    TP 5 ítems 4, 5 y 6 terminados

--- Assets/Scripts/ArrayFamilia.cs
+++ Assets/Scripts/ArrayFamilia.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ArrayFamilia : MonoBehaviour
 {
     public int[] edadesFamilia;
+    int sumaEdades = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +21,33 @@ public class ArrayFamilia : MonoBehaviour
             Debug.Log(edadesFamilia[i]);
         }
 
+        // Escribir el promedio de los elementos del array
+        for (int i = 0; i < edadesFamilia.Length; i++)
+        {
+            sumaEdades += edadesFamilia[i];
+        }
+        float promedio = sumaEdades / edadesFamilia.Length;
+        Debug.Log(promedio);
+
         // Escribir los elementos del array mayores que 20
-        for (int i = 0; i > 20; i++)
+        for (int i = 0; i < edadesFamilia.Length; i++)
+        {
+            if (edadesFamilia[i] > 20)
+            {
+                Debug.Log(edadesFamilia[i]);
+            }
+        }
+
+        // Escribir los elementos del array en orden inverso
+        for (int i = edadesFamilia.Length - 1; i >= 0; i--)
         {
             Debug.Log(edadesFamilia[i]);
         }
+    }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
         {
 
         }
-    }
 }
